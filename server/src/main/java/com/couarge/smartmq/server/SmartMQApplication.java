b0b477ed3d3cb29a29603200d5781b9b079f69c1
makemyownlife <zhangyong7120180@163.com>
2023-11-14 22:34:54 +0800

    初始化Main函数

--- server/src/main/java/com/couarge/smartmq/server/SmartMQApplication.java
+++ server/src/main/java/com/couarge/smartmq/server/SmartMQApplication.java
@@ -1,9 +1,16 @@
 package com.couarge.smartmq.server;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class SmartMQApplication {
 
+    private final static Logger logger = LoggerFactory.getLogger(SmartMQApplication.class);
+
     public static void main(String[] args) {
-        System.out.println("hello ,world");
+        long start = System.currentTimeMillis();
+        logger.info("开始启动SmartMQ服务");
+        logger.info("结束启动SmartMQ服务,耗时：" + (System.currentTimeMillis() - start) + "毫秒");
     }
 
 }
