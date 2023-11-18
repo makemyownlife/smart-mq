92a37686334c5cafc4ebf7048445efa944bc9669
makemyownlife <zhangyong7120180@163.com>
2023-11-18 17:05:17 +0800

    配置 & 控制器

---
+++ server/src/main/java/com/couarge/smartmq/server/SmartMQConfig.java
@@ -0,0 +1,11 @@
+package com.couarge.smartmq.server;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SmartMQConfig {
+    private final static Logger logger = LoggerFactory.getLogger(SmartMQConfig.class);
+
+
+
+}
---
+++ server/src/main/java/com/couarge/smartmq/server/SmartMQController.java
@@ -0,0 +1,13 @@
+package com.couarge.smartmq.server;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SmartMQController {
+
+    private final static Logger logger = LoggerFactory.getLogger(SmartMQController.class);
+
+
+
+
+}
