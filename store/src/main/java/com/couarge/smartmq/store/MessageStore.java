4da006915aeb17f83e6cddc5d3ea57558ceae7e7
makemyownlife <zhangyong7120180@163.com>
2023-11-24 09:41:01 +0800

    [新增代码] 消息存储接口类

---
+++ store/src/main/java/com/couarge/smartmq/store/MessageStore.java
@@ -0,0 +1,29 @@
+package com.couarge.smartmq.store;
+
+public interface MessageStore {
+
+    /**
+     * Load previously stored messages.
+     *
+     * @return true if success; false otherwise.
+     */
+    boolean load();
+
+    /**
+     * Launch this message store.
+     *
+     * @throws Exception if there is any error.
+     */
+    void start() throws Exception;
+
+    /**
+     * Shutdown this message store.
+     */
+    void shutdown();
+
+    /**
+     * Destroy this message store. Generally, all persistent files should be removed after invocation.
+     */
+    void destroy();
+
+}
--- store/src/main/java/com/couarge/smartmq/store/OffsetStore.java
+++
@@ -1,7 +0,0 @@
-package com.couarge.smartmq.store;
-
-public interface OffsetStore {
-
-
-
-}
