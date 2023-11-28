70150f19b8f2cb6728f1a7cb48732cd94189dd74
makemyownlife <zhangyong7120180@163.com>
2023-11-29 00:03:31 +0800

    [添加代码] RockDB 相关测试用例

--- broker/src/main/java/com/couarge/smartmq/server/SmartMQApplication.java
+++ broker/src/main/java/com/couarge/smartmq/server/SmartMQApplication.java
@@ -5,10 +5,12 @@ import org.slf4j.LoggerFactory;
 
 public class SmartMQApplication {
     private final static Logger logger = LoggerFactory.getLogger(SmartMQApplication.class);
+
     public static void main(String[] args) {
         long start = System.currentTimeMillis();
         logger.info("开始启动SmartMQ服务");
-
+        SmartMQController smartMQController = new SmartMQController();
+        smartMQController.start();
         logger.info("结束启动SmartMQ服务,耗时：" + (System.currentTimeMillis() - start) + "毫秒");
     }
 
--- store/src/test/java/com/courage/smartmq/store/test/RocksDBUnitTest.java
+++ store/src/test/java/com/courage/smartmq/store/test/RocksDBUnitTest.java
@@ -1,4 +1,18 @@
 package com.courage.smartmq.store.test;
 
+import org.junit.Test;
+import org.rocksdb.RocksDB;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class RocksDBUnitTest {
+
+    private static final Logger LOG = LoggerFactory.getLogger(RocksDBUnitTest.class);
+
+    static {
+        RocksDB.loadLibrary();
+    }
+
+    
+
 }
