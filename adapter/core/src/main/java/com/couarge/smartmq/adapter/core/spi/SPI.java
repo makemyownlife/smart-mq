74a2cce56899e1a3c1f4cc9ac609340d6ea26cb2
zhangyong <zhangyong7120180@163.com>
2023-12-20 09:41:28 +0800

    [添加代码] 添加 SPI 类

---
+++ adapter/core/src/main/java/com/couarge/smartmq/adapter/core/spi/SPI.java
@@ -0,0 +1,15 @@
+package com.couarge.smartmq.adapter.core.spi;
+
+import java.lang.annotation.*;
+
+/**
+ * SPI装载器注解
+ **/
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.TYPE })
+public @interface SPI {
+
+    // Default SPI name
+    String value() default "";
+}
