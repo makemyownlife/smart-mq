97a0384421102757e588a16f9382559c1fc57ad3
zhangyong <zhangyong7120180@163.com>
2023-12-30 19:22:57 +0800

    [修改代码] 添加 HA 高可用服务

---
+++ broker/src/main/java/cn/itcourage/smartmq/broker/SmartMQHaService.java
@@ -0,0 +1,14 @@
+package cn.itcourage.smartmq.broker;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 高可用服务
+ */
+public class SmartMQHaService {
+
+    private final static Logger logger = LoggerFactory.getLogger(SmartMQHaService.class);
+
+
+}
