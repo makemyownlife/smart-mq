ce7106c9e8708d0664d8ca7ffc8cc1742b0fda73
zhangyong <zhangyong7120180@163.com>
2024-01-30 18:39:06 +0800

    [优化代码] 本地文件偏移量存储

---
+++ store/src/main/java/cn/itcourage/smartmq/store/LocalFileOffsetStore.java
@@ -0,0 +1,12 @@
+package cn.itcourage.smartmq.store;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LocalFileOffsetStore implements OffsetStore {
+
+    private final static Logger logger = LoggerFactory.getLogger(LocalFileOffsetStore.class);
+
+    
+
+}
---
+++ store/src/main/java/cn/itcourage/smartmq/store/OffsetStore.java
@@ -0,0 +1,14 @@
+package cn.itcourage.smartmq.store;
+
+import cn.itcourage.smartmq.store.MessageStore;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 消息偏移量存储
+ */
+public interface OffsetStore {
+
+
+
+}
